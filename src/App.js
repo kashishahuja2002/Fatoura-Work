cd4258402de206945cd58141cc5aed730eca6f03
kashishahuja2002 <kashishahuja2002@gmail.com>
2022-06-08 19:40:56 +0530

    Implemented routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import HomeContainer from './components/pages/HomeContainer'
-
-// let dummyData = [
-//     {"abc@abc.com": "abc"}, 
-//     {"xyz@xyz.com": "xyz"}
-// ];
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from './components/auth/Auth';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+import HomeContainer from './components/pages/HomeContainer';
+import Dashboard from './components/pages/Dashboard';
+import Documents from './components/pages/Documents';
+import Reports from './components/pages/Reports';
+import Create from './components/pages/CreateNew';
 
 function App() {
     return (
-        <HomeContainer />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth/" element={<Auth />}>
+                    <Route path="signup" element={<Signup />} />
+                    <Route path="login" element={<Login />} />
+                </Route>
+            </Routes>
+            <Routes>
+                <Route path="/pages/" element={<HomeContainer />}>
+                    <Route index path="dashboard" element={<Dashboard />} />
+                    <Route path="myInvoices" element={<Documents />} />
+                    <Route path="myReports" element={<Reports />} />
+                    <Route path="create" element={<Create />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
