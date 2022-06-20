54ea0b7ccaa8a982d7b0ee62271f5fd32ed4e7e0
kashishahuja2002 <kashishahuja2002@gmail.com>
2022-06-20 12:45:49 +0530

    Added route for subscribe-page

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,27 +1,41 @@
-import { React } from "react";
+import { React, useEffect } from "react";
 import './Auth.css';
 import FatouraLogoDark from "../../assets/images/Fatoura-Logo-Dark.png";
 import { Container, Col, Row} from "reactstrap";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import sectionImage from "../../assets/images/section3.3.svg";
+import { useDispatch } from 'react-redux';
+import { getUser } from '../profile/Redux/profileActions';
 
 const Auth = () => {
+    const location = useLocation().pathname.split('/')[2];
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getUser("/users/getUser"));
+    },[]);
+    
     return (
-        <Container fluid>
-            <Row>
-                <Col xs={12} md={6}>
-                    <div className="left-container">
-                        <img src={FatouraLogoDark} alt="Fatoura logo" className="logo" />
-                        <Outlet />
-                    </div>
-                </Col>
-                <Col md={6} className="d-none d-md-block">
-                    <div className="right-container">
-                        <img src={sectionImage} alt="Section" />
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            {location !== "subscribe-plan" 
+                ?<Container fluid>
+                    <Row>
+                        <Col xs={12} md={6}>
+                            <div className="left-container">
+                                <img src={FatouraLogoDark} alt="Fatoura logo" className="logo" />
+                                <Outlet />
+                            </div>
+                        </Col>
+                        <Col md={6} className="d-none d-md-block">
+                            <div className="right-container">
+                                <img src={sectionImage} alt="Section" />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+                :<Outlet />
+            }
+        </>
     );
 }
 
