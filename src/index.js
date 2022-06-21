9acd15b569bf7ad51d0a9615153859edd5c6dd0f
kashishahuja2002 <kashishahuja2002@gmail.com>
2022-06-21 14:03:02 +0530

    Removed strict mode

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
