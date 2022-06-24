008b1d9df5a84bfb736c5a477f1dba9e0ec7647c
kashishahuja2002 <kashishahuja2002@gmail.com>
2022-06-24 19:30:42 +0530

    Updated to include pages reducer

--- src/RootReducer.js
+++ src/RootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from "./components/auth/Redux/authReducer";
 import profileReducer from "./components/profile/Redux/profileReducer";
+import pagesReducer from "./components/pages/Redux/pagesReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    profile: profileReducer
+    profile: profileReducer,
+    pages: pagesReducer
 });
 
 export default rootReducer;
