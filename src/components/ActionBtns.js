0574b0793f06e86bc78088af44c69f14a20a4fca
kashishahuja2002 <kashishahuja2002@gmail.com>
2022-06-29 13:31:42 +0530

    Added delete, edit, and view functionalities

--- src/components/ActionBtns.js
+++ src/components/ActionBtns.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const ActionBtns = () => {
+const ActionBtns = (props) => {
     const navigate = useNavigate();
 
-    const handleEdit = () => {
-        navigate('/pages/createEdit?task=edit&type=Invoice');
+    const handleEdit = (e) => {
+        navigate('/pages/createEdit?task=edit&type=Invoice&id='+e.target.id);
     }
 
-    const handleView = () => {
-        navigate('/pages/view?type=Invoice');
+    const handleView = (e) => {
+        navigate('/pages/view?type=Invoice&id='+e.target.id);
     }
 
     return(
         <div>
-            <button className="action-btn" onClick={handleEdit}>
+            <button className="action-btn" onClick={handleEdit} id={props.id}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
             </button>
-            <button className="action-btn" onClick={handleView}>
+            <button className="action-btn" onClick={handleView} id={props.id}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
             </button>
         </div>
--- src/components/pages/Documents.js
+++ src/components/pages/Documents.js
@@ -3,16 +3,31 @@ import { Container, Row, Dropdown, Button } from "react-bootstrap";
 import './Documents.css';
 import InvoiceTable from "../InvoiceTable";
 import { useDispatch } from "react-redux";
-import { getInvoices, getDeletedInvoices } from "./Redux/documentsAction";
+import { getInvoices, getDeletedInvoices, deleteInvoice } from "./Redux/documentsAction";
 
 const Documents = () => {
     const dispatch = useDispatch();
-    const [docType, setDocType] = useState('all');
+    const [docType, setDocType] = useState('All Documents');
 
     useEffect(() => {
         dispatch(getInvoices());
         dispatch(getDeletedInvoices());
     }, [])
+
+    const [checkedDocs, setCheckedDocs] = useState([]);
+
+    const handleCopy = () => {
+        
+    }
+
+    const handleDelete = () => {
+        for(let i=0; i<checkedDocs.length; i++) {
+            if(checkedDocs[i] != undefined) {
+                dispatch(deleteInvoice(checkedDocs[i]));
+            }
+        }
+        document.getElementById('rootCheckBox').checked = false;
+    }
 
     return (
         <Container fluid className="documents">
@@ -22,22 +37,22 @@ const Documents = () => {
                         <h5>My Invoices</h5>
                         <div className="dropdowns-div">
                             <Dropdown align="end" className="white-dropdown">
-                                <Dropdown.Toggle id="dropdown-basic">
-                                    All Documents 
+                                <Dropdown.Toggle id="dropdown-basic" style={{textTransform: "capitalize"}}>
+                                    {docType}
                                 </Dropdown.Toggle>
                                 <Dropdown.Menu>
-                                    <Dropdown.Item onClick={() => {setDocType('all')}}>All Documents</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => {setDocType('All Documents')}}>All Documents</Dropdown.Item>
                                     <Dropdown.Item onClick={() => {setDocType('overdue')}}>Overdue</Dropdown.Item>
                                     <Dropdown.Item onClick={() => {setDocType('paid')}}>Paid</Dropdown.Item>
                                     <Dropdown.Item onClick={() => {setDocType('unpaid')}}>Unpaid</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => {setDocType('deleted')}}>Waste Bin</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => {setDocType('Waste Bin')}}>Waste Bin</Dropdown.Item>
                                 </Dropdown.Menu>
                             </Dropdown>
-                            <Button className="blue-button">
+                            <Button className="blue-button" onClick={handleCopy}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width={17} fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4 me-2"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"></path></svg>
                                 Copy
                             </Button>
-                            <Button className="blue-button">
+                            <Button className="blue-button" onClick={handleDelete}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width={17} fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4 me-2"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                                 Delete
                             </Button>
@@ -46,7 +61,7 @@ const Documents = () => {
                 </Row>
 
                 <Row>
-                    <InvoiceTable docType={docType} />
+                    <InvoiceTable docType={docType} setCheckedDocs={setCheckedDocs} />
                 </Row>
 
             </div>
