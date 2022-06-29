a39f4171999e2840ab2b056a0ee799c7ae470214
kashishahuja2002 <kashishahuja2002@gmail.com>
2022-06-29 13:36:20 +0530

    Update View.js

--- src/components/pages/View.js
+++ src/components/pages/View.js
@@ -7,6 +7,7 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 const View = () => {
     const [searchParams] = useSearchParams();
     const type = searchParams.get('type');
+    const id = searchParams.get('id');
 
     const navigate = useNavigate();
 
@@ -29,7 +30,7 @@ Invoice Billing Address`;
                             </Button>
                         </div>
                         <div>
-                            <Button className="blue-button"  onClick={() => navigate('/pages/createEdit?task=edit&type=Invoice')}>
+                            <Button className="blue-button"  onClick={() => navigate('/pages/createEdit?task=edit&type=Invoice&id='+id)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width={17} fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4 me-2"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
                                 Edit
                             </Button>
